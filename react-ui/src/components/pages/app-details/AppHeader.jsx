296b21dd0f70449c28021db04fe5b60ad313b4c4
BasMeijer <mail@basmeijer.com>
2017-11-28 15:53:32 +0100

    added basic app create page, removed unused components and refactored the component layout to feature pages

--- react-ui/src/components/pages/app-details/AppHeader.jsx
+++ react-ui/src/components/pages/app-details/AppHeader.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import NavigationBar from '../navigation/NavigationBar';
+import NavigationBar from '../../navigation/NavigationBar';
 
 const AppHeader = (props) => {
   return (
