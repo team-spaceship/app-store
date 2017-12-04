a09a639555b162398e0e6f2c6d9d765b3484101d
Oteken <Otekenpekince@hotmail.com>
2017-12-04 12:20:59 +0100

    Add: Fields added into Version entity inside mongodb. Version entity now adopts fields app_downloads, app_ratings fields from App entity(with different names).

--- src/schemas/Version.js
+++ src/schemas/Version.js
@@ -2,8 +2,12 @@ import mongoose from 'mongoose';
 
 const VersionSchema = new mongoose.Schema({
   version: String,
-  file_name: String,
+  description: String,
+  version_path: String,
+  version_note: String,
   app: { type: mongoose.Schema.Types.ObjectId, ref: 'App', required: true },
+  version_downloads: [{ type: mongoose.Schema.Types.ObjectId, ref: 'InstalledApp', required: false }],
+  version_ratings: [{ type: mongoose.Schema.Types.ObjectId, ref: 'AppRating', required: false }],
   publish_reviews: [{ type: mongoose.Schema.Types.ObjectId, ref: 'PublishReview', required: false }],
 }, {
   timestamps: true,
