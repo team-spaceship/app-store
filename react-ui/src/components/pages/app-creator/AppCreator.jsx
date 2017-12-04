b69b3101f8e18c58bc2707906db34f0fb4525f88
BasMeijer <mail@basmeijer.com>
2017-12-04 15:03:42 +0100

    added create app form including validation with formik plugin

--- react-ui/src/components/pages/app-creator/AppCreator.jsx
+++ react-ui/src/components/pages/app-creator/AppCreator.jsx
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import AppForm from './AppForm';
+import NavigationBar from '../../navigation/NavigationBar';
 
 class AppCreator extends Component {
   constructor() {
@@ -9,13 +11,28 @@ class AppCreator extends Component {
       temporaryTitle: "Create Lumos App",
     };
   }
+
+  handleFormSubmit(app) {
+    // temporary untill backend call to create an app is present
+    console.log(app);
+  }
+
   render() {
     return (
-      <div>
-        <h2>{this.state.temporaryTitle}</h2>
+      <div className="container">
+        <NavigationBar />
+
+        <div className="form-container"> 
+          <h2>{this.state.temporaryTitle}</h2>
+          <p className="form-information">
+            Please provide us with the information necessary Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque facere beatae nostrum laudantium unde sunt quidem. Fugiat explicabo eum facilis eius, deleniti rerum similique, at error natus consequuntur saepe accusamus?
+          </p>
+          <AppForm handleFormSubmit={this.handleFormSubmit} app={{ appname: 'hodor', version: '1', description: '', apprepository: '' }} />
+        </div>
       </div>
     );
   }
+
 }
 
 AppCreator.propTypes = {
