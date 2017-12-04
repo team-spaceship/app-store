76c7c2759bedd5c6a2200c981e681fe66556163a
Oteken <Otekenpekince@hotmail.com>
2017-12-04 13:10:22 +0100

    Change: AppRating and InstalledApp schemas were renamed to VersionRating and InstalledVersion respectively. References to these files were renamed to the correct one.

--- src/schemas/Version.js
+++ src/schemas/Version.js
@@ -6,8 +6,8 @@ const VersionSchema = new mongoose.Schema({
   version_path: String,
   version_note: String,
   app: { type: mongoose.Schema.Types.ObjectId, ref: 'App', required: true },
-  version_downloads: [{ type: mongoose.Schema.Types.ObjectId, ref: 'InstalledApp', required: false }],
-  version_ratings: [{ type: mongoose.Schema.Types.ObjectId, ref: 'AppRating', required: false }],
+  version_downloads: [{ type: mongoose.Schema.Types.ObjectId, ref: 'InstalledVersion', required: false }],
+  version_ratings: [{ type: mongoose.Schema.Types.ObjectId, ref: 'VersionRating', required: false }],
   publish_reviews: [{ type: mongoose.Schema.Types.ObjectId, ref: 'PublishReview', required: false }],
 }, {
   timestamps: true,
