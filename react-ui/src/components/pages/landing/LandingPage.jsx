ff71a756e00795c40549666bab742a7437c88bb4
Your NameDominic <dominichoogkamer@gmail.com>
2017-12-11 00:59:19 +0100

    CSS of the landing page

--- react-ui/src/components/pages/landing/LandingPage.jsx
+++ react-ui/src/components/pages/landing/LandingPage.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Header from "../../header/Header";
+import LightBulb from '../../../images/LightBulb.png';
 import AppCard from "../../app-card/AppCard";
 
 import "./landingpage.css";
@@ -14,16 +15,15 @@ const LandingPage = () => {
             <div className="wrapper--box">
               <div className="box--border">
                 <ul>
-                  <li><h5>OUT</h5></li>
-                  <li><h5>OF</h5></li>
-                  <li><h5>THE</h5></li>
-                  <li><h5>BOX</h5></li>
+                  <li><h5>OUT OF THE BOX</h5></li>
                 </ul>
               </div>
             </div>
             <div className="box--thinking">
               <h2>HOW WE THINK</h2>
               <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur iste enim nihil tempora ex nam rem beatae, cumque cupiditate quia magni vitae cum veniam. Quam quia illum ad dolorem, tempore nesciunt quasi accusantium ex fugiat, magni sapiente quos. Cum harum ducimus dignissimos maxime maiores quidem dolorum nostrum delectus, beatae et.</p>
+              <img src="https://static1.squarespace.com/static/52f7dbf3e4b0a5a45c1a6bdd/57e6a2ae6b8f5b926163c53b/57e6a2f129687f1c49bd5e14/1474733830099/kickstarter-logo-black.png" alt="" />
+              <div className="lightbar" />
             </div>
           </section>
         </div>
@@ -34,23 +34,23 @@ const LandingPage = () => {
             <div><h2>WHAT'S <br /> INSIDE</h2></div>
             <div>
               <ul>
-                <li>INCLUDES A MOBILE APP</li>
-                <li>INCLUDES A MOBILE APP</li>
-                <li>INCLUDES A MOBILE APP</li>
-                <li>INCLUDES A MOBILE APP</li>
+                <li><img src={LightBulb} alt="lightbulb icon" />INCLUDES A MOBILE APP</li>
+                <li><img src={LightBulb} alt="lightbulb icon" />INCLUDES A MOBILE APP</li>
+                <li><img src={LightBulb} alt="lightbulb icon" />INCLUDES A MOBILE APP</li>
+                <li><img src={LightBulb} alt="lightbulb icon" />INCLUDES A MOBILE APP</li>
               </ul>
             </div>
             <div>
               <ul>
-                <li>INCLUDES A MOBILE APP</li>
-                <li>INCLUDES A MOBILE APP</li>
-                <li>INCLUDES A MOBILE APP</li>
-                <li>INCLUDES A MOBILE APP</li>
+                <li><img src={LightBulb} alt="lightbulb icon" />INCLUDES A MOBILE APP</li>
+                <li><img src={LightBulb} alt="lightbulb icon" />INCLUDES A MOBILE APP</li>
+                <li><img src={LightBulb} alt="lightbulb icon" />INCLUDES A MOBILE APP</li>
+                <li><img src={LightBulb} alt="lightbulb icon" />INCLUDES A MOBILE APP</li>
               </ul>
             </div>
           </section>
         </div>
-        <h1>WHAT'S <br /> INSIDE</h1>
+        <h1 className="bg--text left">WHAT'S <br /> INSIDE</h1>
       </div>
       <div className="wrapper">
         <div className="container">
@@ -65,10 +65,10 @@ const LandingPage = () => {
       <div className="wrapper--bg">
         <div className="container">
           <section className="wrapper--specs">
-            <div><h2>ARE YOU <br /> INSPIRED?</h2></div>
+            <div><h2>ARE YOU <br /> <span>INSPIRED</span> <br /> TO MAKE AN APP?</h2></div>
           </section>
         </div>
-        <h1>ARE YOU <br /> INSPIRED?</h1>
+        <h1 className="bg--text right">ARE YOU <br /> INSPIRED? <br /></h1>
       </div>
     </div >
   );
