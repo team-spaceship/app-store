199603c8cc0ee9b1f1c761ba891e8e71565eb83c
Sven de Ronde <ewderonde@gmail.com>
2018-01-15 16:44:41 +0100

    You can now remove the apps on the admin page. Removing an app will also delete all versions and installedapp rows. (Go to /admin, not /v1/admin)

---
+++ src/routes/adminRoutes.js
@@ -0,0 +1,13 @@
+import { authenticated } from '../middleware/passport';
+import appController from "../controllers/appController";
+
+export default class AdminRoutes {
+  /**
+  * Create the app routes.
+  */
+  static create(router) {
+    router.delete('/v1/admin/app/:id/delete', authenticated, appController.deleteApp);
+
+    router.get('/v1/admin/app/deleteall', authenticated, appController.deleteAllApps);
+  }  
+}
